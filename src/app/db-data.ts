ad0b6b92fb23c436807420c68e35dfe8825edf30
cameaann <cameaann@gmail.com>
2019-12-10 00:58:44 +0300

    edited projects

--- src/app/db-data.ts
+++ src/app/db-data.ts
@@ -14,42 +14,42 @@ export const PROJECTS: any = [
         src: "https://cameaann.github.io/app-rest/",
         longDescription: "Design a simple onboarding flow: in-app screens/ user dashboard/ profile. Final design should include personas, use cases: sketches, wireframes, final prototype beautifully mocked up"
     },
-    // {
-    //     id: 2,
-    //     description: "Blog",
-    //     iconUrl: '',
-    //     longDescription: "Here should be homepage, category page and article page. Here should be sketches, wireframes and working hi-fi prototype. "
-    // },
-    // {
-    //     id: 3,
-    //     description: "Ecommerce Website",
-    //     iconUrl: '',
-    //     longDescription: "Here should be product search page that makes filtering & searching simple and intuitive. Here also should be product detail page? that makes it easy for visitors to gather information about the product & purchase it. Metrics are key focus of any good ecommerce site. Compelling and unique visual look"
-    // },
-    // {
-    //     id: 5,
-    //     description: "Email Drip Campaign",
-    //     iconUrl: '',
-    //     longDescription: "Identify a business client & design series of emails for them, designed to convert new subscribers or trialers into paying customers of their product."
-    // },
-    // {
-    //     id: 6,
-    //     description: "Marketing Website",
-    //     iconUrl: '',
-    //     longDescription: "Show relevant information about the product. The page will need visuals of the product, compelling text to explain product features."
-    // },
-    // {
-    //     id: 7,
-    //     description: "Web App Design",
-    //     iconUrl: '',
-    //     longDescription: "Think of some simple app that solves existing problem. Design the signup flow, in-app screens. Dashboard for the app should include: sketches, wireframes, a hi-fi prototype mocked onto a device."
-    // },
-    // {
-    //     id: 8,
-    //     description: "Graphic Design",
-    //     iconUrl: '',
-    //     longDescription: "Some ideas of posters, postcards, visiting cards, illustrations."
-    // }
+    {
+        id: 2,
+        description: "Blog",
+        iconUrl: '',
+        longDescription: "Here should be homepage, category page and article page. Here should be sketches, wireframes and working hi-fi prototype. "
+    },
+    {
+        id: 3,
+        description: "Ecommerce Website",
+        iconUrl: '',
+        longDescription: "Here should be product search page that makes filtering & searching simple and intuitive. Here also should be product detail page? that makes it easy for visitors to gather information about the product & purchase it. Metrics are key focus of any good ecommerce site. Compelling and unique visual look"
+    },
+    {
+        id: 5,
+        description: "Email Drip Campaign",
+        iconUrl: '',
+        longDescription: "Identify a business client & design series of emails for them, designed to convert new subscribers or trialers into paying customers of their product."
+    },
+    {
+        id: 6,
+        description: "Marketing Website",
+        iconUrl: '',
+        longDescription: "Show relevant information about the product. The page will need visuals of the product, compelling text to explain product features."
+    },
+    {
+        id: 7,
+        description: "Web App Design",
+        iconUrl: '',
+        longDescription: "Think of some simple app that solves existing problem. Design the signup flow, in-app screens. Dashboard for the app should include: sketches, wireframes, a hi-fi prototype mocked onto a device."
+    },
+    {
+        id: 8,
+        description: "Graphic Design",
+        iconUrl: '',
+        longDescription: "Some ideas of posters, postcards, visiting cards, illustrations."
+    }
     // {
     //     id: 9,
     //     description: "Mobile App Design",
